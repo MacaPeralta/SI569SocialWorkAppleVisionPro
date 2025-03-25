64b350a444af60e41bb3cfd0814b9a5d532ece55
jingle chen <47862618+jinglechen2287@users.noreply.github.com>
2025-03-25 15:02:48 -0400

    remove table from the immersive view

--- FinalDiscussionGame/GameImmersiveView.swift
+++ FinalDiscussionGame/GameImmersiveView.swift
@@ -9,39 +9,48 @@ struct GameImmersiveView: View {
 
     var body: some View {
         ZStack {
-            if let loadedGame = game, let tableAnchor = anchor {
-                RealityView { content in
-                    content.add(tableAnchor)
-
-                    loadedGame.renderer.root.transform.translation = .zero
-                    tableAnchor.addChild(loadedGame.renderer.root)
-
-                    if let texture = try? await TextureResource(named: "360image") {
-                        var material = UnlitMaterial()
-                        material.color = .init(texture: .init(texture))
-                        let sphere = ModelEntity(mesh: .generateSphere(radius: 1000), materials: [material])
-                        sphere.scale = [-1, 1, 1]
-                        loadedGame.renderer.root.addChild(sphere)
-                    }
-
-                    print("Game added to anchor.")
+            RealityView { content in
+                if let texture = try? await TextureResource(named: "360image") {
+                    var material = UnlitMaterial()
+                    material.color = .init(texture: .init(texture))
+                    let sphere = ModelEntity(mesh: .generateSphere(radius: 1000), materials: [material])
+                    sphere.scale = [-1, 1, 1]
+                    content.add(sphere)
                 }
-                // Unwrapped game + anchor here
-                .tabletopGame(loadedGame.tabletopGame, parent: tableAnchor) { _ in
-                    GameInteraction(game: loadedGame)
-                }
-
-            } else {
-                // Load game + anchor
-                Color.clear
-                    .task {
-                        print("Loading game + anchor...")
-                        let newGame = await Game()
-                        game = newGame
-                        anchor = AnchorEntity(.world(transform: matrix_identity_float4x4))
-                        print("Game and anchor loaded.")
-                    }
             }
+//            if let loadedGame = game, let tableAnchor = anchor {
+//                RealityView { content in
+//                    content.add(tableAnchor)
+//
+//                    loadedGame.renderer.root.transform.translation = .zero
+//                    tableAnchor.addChild(loadedGame.renderer.root)
+//
+//                    if let texture = try? await TextureResource(named: "360image") {
+//                        var material = UnlitMaterial()
+//                        material.color = .init(texture: .init(texture))
+//                        let sphere = ModelEntity(mesh: .generateSphere(radius: 1000), materials: [material])
+//                        sphere.scale = [-1, 1, 1]
+//                        loadedGame.renderer.root.addChild(sphere)
+//                    }
+//
+//                    print("Game added to anchor.")
+//                }
+//                // Unwrapped game + anchor here
+//                .tabletopGame(loadedGame.tabletopGame, parent: tableAnchor) { _ in
+//                    GameInteraction(game: loadedGame)
+//                }
+//
+//            } else {
+//                // Load game + anchor
+//                Color.clear
+//                    .task {
+//                        print("Loading game + anchor...")
+//                        let newGame = await Game()
+//                        game = newGame
+//                        anchor = AnchorEntity(.world(transform: matrix_identity_float4x4))
+//                        print("Game and anchor loaded.")
+//                    }
+//            }
         }
     }
 }

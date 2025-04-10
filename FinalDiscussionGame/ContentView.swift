76e24cec7bf81edc0614f7a18c184707f3f74d2c
Kavya Jeganathan <kavyaje08@gmail.com>
2025-04-10 13:29:11 -0400

    Updated countdown timer time

--- FinalDiscussionGame/ContentView.swift
+++ FinalDiscussionGame/ContentView.swift
@@ -306,7 +306,7 @@ struct ContentView: View {
 
         // Timer Logic
         func startTimer() {
-            countdown = 30 // 20 minutes in seconds
+            countdown = 300 // 20 minutes in seconds
             timerIsActive = true
             Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
                 DispatchQueue.main.async {
